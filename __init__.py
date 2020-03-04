13520b279a5305aa25a3e4d82e3beccb555d39df
Henrik Giesel <hengiesel@gmail.com>
2020-03-04 12:45:16 +0100

    Move credits to init file

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+# Straight Reward:
+# an Anki addon increases Ease Factor at straight success
+# ("Good" or "Easy" rating in review).
+# GitHub: https://github.com/luminousspice/anki-addons/
+#         https://github.com/hgiesel/anki_straight_reward/
+#
+# Copyright: 2019 Luminous Spice <luminous.spice@gmail.com>
+#            2020 Henrik Giesel <hengiesel@gmail.com>
+#
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/copyleft/agpl.html
+
 from aqt import mw
 import os.path as path
 
--- src/main.py
+++ src/main.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-# Straight Reward:
-# an Anki addon increases Ease Factor at every 5 straight success
-# ("Good" or "Easy" rating in review).
-# GitHub: https://github.com/luminousspice/anki-addons/
-#
-# Copyright: 2019 Luminous Spice <luminous.spice@gmail.com>
-#            2020 Henrik Giesel <hengiesel@gmail.com>
-# License: GNU AGPL, version 3 or later; http://www.gnu.org/copyleft/agpl.html
-
 from .gui.gui_conf import init_conf
 from .lib.review_hook import init_review_hook
 from .lib.sync_hook import init_sync_hook
