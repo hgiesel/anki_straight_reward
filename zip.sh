0106751f347d8c809989a570cdc3c7ce58211c87
Henrik Giesel <hengiesel@gmail.com>
2020-03-04 12:45:16 +0100

    Add skeleton

---
+++ zip.sh
@@ -0,0 +1,21 @@
+declare DIR=${BASH_SOURCE%/*}
+
+npm run --prefix "${DIR}/js" build
+
+if [[ "$1" == '-a' ]]; then
+  # for uploading to AnkiWeb
+  declare addon_id='??'
+else
+  # for installing myself
+  declare addon_id='straight_reward'
+fi
+
+rm -f "${DIR}/${addon_id}.ankiaddon"
+
+zip -r "${DIR}/${addon_id}.ankiaddon" \
+  "${DIR}/config."{json,md} \
+  "${DIR}/manifest.json" \
+  "${DIR}/__init__.py" \
+  "${DIR}/src/"*".py" \
+  "${DIR}/src/lib/"*".py" \
+  "${DIR}/src/gui_config/"*".py" \
