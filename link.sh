c689e57e8521d35da0a85c0167f951853cddc7a8
Henrik Giesel <hengiesel@gmail.com>
2020-05-22 17:42:16 +0200

    Update link script

--- link.sh
+++ link.sh
@@ -3,9 +3,9 @@ declare DIR=$(realpath ${BASH_SOURCE%/*})
 declare addon_name="StraightRewardDev"
 declare customdir=''
 
-if [[ "$1" =~ ^d ]]; then
-  if [[ "$customdir" ]]; then
-    rm "$customdir/$name"
+if [[ "$1" =~ ^d || "$1" =~ ^-d ]]; then
+  if [[ -d "$customdir" ]]; then
+    rm "$customdir/$addon_name"
 
   elif [[ -d "$HOME/.local/share/AnkiDev/addons21" ]]; then
     rm "$HOME/.local/share/AnkiDev/addons21/$addon_name"
@@ -21,8 +21,8 @@ if [[ "$1" =~ ^d ]]; then
     exit -1
   fi
 
-elif [[ "$1" =~ ^c ]]; then
-  if [[ "$customdir" ]]; then
+elif [[ "$1" =~ ^c || "$1" =~ ^-c ]]; then
+  if [[ -d "$customdir" ]]; then
     ln -s "$DIR" "$customdir/$addon_name"
 
   elif [[ -d "$HOME/.local/share/AnkiDev/addons21" ]]; then
@@ -38,4 +38,8 @@ elif [[ "$1" =~ ^c ]]; then
     echo 'Unknown platform'
     exit -1
   fi
+
+else
+  echo 'Unknown command'
+  exit -2
 fi
