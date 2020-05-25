2335e425a5bae5730c2be1c35537df739be9a358
Henrik Giesel <hengiesel@gmail.com>
2020-05-25 11:19:05 +0200

    Change meta.json writing

--- __init__.py
+++ __init__.py
@@ -11,18 +11,28 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/copyleft/agpl.html
 
 from aqt import mw
-import os.path as path
+from os import path
+import json
 
 # Write manifest.json to config.json
 if mw.addonManager.addonName(path.dirname(__file__)) != 'Straight Reward':
     dir_path = path.dirname(path.realpath(__file__))
 
     with open(path.join(dir_path, 'manifest.json')) as f:
-        if path.exists('meta.json'):
+        config_data = {}
+
+        try:
             with open(path.join(dir_path, 'meta.json')) as f_old:
-                import json
-                mw.addonManager.writeAddonMeta(dir_path, json.load(f).update({
-                    'config': json.load(f_old)['config']
-                }))
+                config_data = json.load(f_old)['config']
+
+        except FileNotFoundError:
+            pass
+
+        meta = json.load(f)
+        meta.update({
+            'config': config_data,
+        })
+
+        mw.addonManager.writeAddonMeta(dir_path, meta)
 
 from .src import main
