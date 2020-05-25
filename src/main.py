7019e058583c6cb72c54ca390cab273e2f073722
Henrik Giesel <hengiesel@gmail.com>
2020-05-25 11:11:06 +0200

    Actually execute warning code

--- src/main.py
+++ src/main.py
@@ -3,14 +3,16 @@ from aqt.utils import showInfo
 
 def show_info():
     showInfo(
-        'The configuration via config.json is phased out since Straight Reward v0.4.\n'
+        'The configuration via config.json is phased out since Straight Reward v0.4.\n\n'
         'To configure the functionality of this add-on, go to the Deck Overview, '
-        'choose a Deck, click the wheel to its right, select "Options", and in resulting window '
+        'choose a Deck, click the wheel to its right, select "Options", and in the resulting window '
         'open the "Rewards" tab.'
     )
 
 def setup_addon_manager():
     mw.addonManager.setConfigAction(__name__, show_info)
+
+setup_addon_manager()
 
 from .gui.deckconf import init_conf
 from .lib.review_hook import init_review_hook
