e8bc21b1a9bd8cf0738deaf731f8af7a66757076
Henrik Giesel <hengiesel@gmail.com>
2020-05-27 14:04:14 +0200

    Name meta.json instead of config.json

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from aqt.utils import showInfo
 
 def show_info():
     showInfo(
-        'The configuration via config.json is phased out since Straight Reward v0.4.\n\n'
+        'The configuration via meta.json is phased out since Straight Reward v0.4.\n\n'
         'To configure the functionality of this add-on, go to the Deck Overview, '
         'choose a Deck, click the wheel to its right, select "Options", and in the resulting window '
         'open the "Rewards" tab.'
