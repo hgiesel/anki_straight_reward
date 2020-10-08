bef092165c2491e143e017d8aff2867cfd3aa8dd
Henrik Giesel <hengiesel@gmail.com>
2020-10-08 15:59:30 +0200

    Update link script

--- bin/link.sh
+++ bin/link.sh
@@ -3,42 +3,31 @@ declare DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
 declare addon_name="StraightRewardDev"
 declare customdir=''
 
+if [[ -d "$customdir" ]]; then
+  declare target="$customdir/$addon_name"
+
+elif [[ -d "$HOME/.local/share/AnkiDev/addons21" ]]; then
+  declare target="$HOME/.local/share/AnkiDev/addons21/$addon_name"
+
+elif [[ $(uname) = 'Darwin' ]]; then
+  declare target="$HOME/Library/Application Support/Anki2/addons21/$addon_name"
+
+elif [[ $(uname) = 'Linux' ]]; then
+  declare target="$HOME/.local/share/Anki2/addons21/$addon_name"
+
+else
+  echo 'Unknown platform'
+  exit -1
+fi
+
 if [[ "$1" =~ ^-?d$ ]]; then
-  if [[ -d "$customdir" ]]; then
-    rm "$customdir/$addon_name"
-
-  elif [[ -d "$HOME/.local/share/AnkiDev/addons21" ]]; then
-    rm "$HOME/.local/share/AnkiDev/addons21/$addon_name"
-
-  elif [[ $(uname) = 'Darwin' ]]; then
-    rm "$HOME/Library/Application Support/Anki2/addons21/$addon_name"
-
-  elif [[ $(uname) = 'Linux' ]]; then
-    rm "$HOME/.local/share/Anki2/addons21/$addon_name"
-
+  if [[ ! -h "$target" ]]; then
+    echo 'Directory was not linked'
   else
-    echo 'Unknown platform'
-    exit -1
+    rm "$target"
   fi
 
 elif [[ "$1" =~ ^-?c$ ]]; then
-  if [[ -d "$customdir" ]]; then
-    declare target="$customdir/$addon_name"
-
-  elif [[ -d "$HOME/.local/share/AnkiDev/addons21" ]]; then
-    declare target="$HOME/.local/share/AnkiDev/addons21/$addon_name"
-
-  elif [[ $(uname) = 'Darwin' ]]; then
-    declare target="$HOME/Library/Application\ Support/Anki2/addons21/$addon_name"
-
-  elif [[ $(uname) = 'Linux' ]]; then
-    declare target="$HOME/.local/share/Anki2/addons21/$addon_name"
-
-  else
-    echo 'Unknown platform'
-    exit -1
-  fi
-
   if [[ ! -h "$target" ]]; then
     ln -s "$DIR" "$target"
   else
