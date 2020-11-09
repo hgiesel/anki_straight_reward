20f785243f537fa27390d0c823fbe2e0a1985e20
Henrik Giesel <hengiesel@gmail.com>
2020-11-09 23:20:10 +0100

    Avoid explicit closure function in review_hook

--- src/lib/review_hook.py
+++ src/lib/review_hook.py
@@ -34,45 +34,38 @@ def from_rescheduling_deck(col: Collection, card: Card) -> bool:
     # check whether it is a filtered deck ("dynamic") which does not reschedule
     return not col.decks.isDyn(card.did) or mw.col.decks.get(card.did)['resched']
 
-def review_hook_closure():
-    gains = {}
-
-    def check_straight_reward(ease_tuple: Tuple[bool, int], _reviewer, card: Card) -> Tuple[bool, int]:
-        nonlocal gains
-
-        if not card_success(card, ease_tuple[1]) or not from_rescheduling_deck(mw.col, card):
-            return ease_tuple
-
-        # plus one for the current success
-        straightlen = get_straight_len(mw.col, card.id) + 1
-        easeplus = get_easeplus(mw.col, card, straightlen)
-
-        if not easeplus:
-            return ease_tuple
-
-        gains[card.id] = easeplus
-
-        if notifications_enabled(mw.col, card):
-            display_success(straightlen, int(easeplus / 10))
-
+def check_straight_reward(gains: dict, ease_tuple: Tuple[bool, int], card: Card) -> Tuple[bool, int]:
+    if not card_success(card, ease_tuple[1]) or not from_rescheduling_deck(mw.col, card):
         return ease_tuple
 
-    def flush_with_straight_reward(card: Card):
-        nonlocal gains
+    # plus one for the current success
+    straightlen = get_straight_len(mw.col, card.id) + 1
+    easeplus = get_easeplus(mw.col, card, straightlen)
 
-        if card.id not in gains:
-            return
+    if not easeplus:
+        return ease_tuple
 
-        card.factor += gains[card.id]
-        del gains[card.id]
+    gains[card.id] = easeplus
 
-    return (
-        check_straight_reward,
-        flush_with_straight_reward,
-    )
+    if notifications_enabled(mw.col, card):
+        display_success(straightlen, int(easeplus / 10))
+
+    return ease_tuple
+
+def flush_with_straight_reward(gains: dict, card: Card) -> None:
+    if card.id not in gains:
+        return
+
+    card.factor += gains[card.id]
+    del gains[card.id]
 
 def init_review_hook():
-    check, flush = review_hook_closure()
+    gains = {}
 
-    reviewer_will_answer_card.append(check)
-    card_will_flush.append(flush)
+    reviewer_will_answer_card.append(
+        lambda ease_tuple, _reviewer, card: check_straight_reward(gains, ease_tuple, card),
+    )
+
+    card_will_flush.append(
+        lambda card: flush_with_straight_reward(gains, card),
+    )
